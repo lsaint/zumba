00a7fbb448695f4e55e3190dd1767e8417e761f8
Ethan St. Lee <ethan@huocc.com>
2016-05-12 17:56:31 +0800

    admin

--- voting/admin.py
+++ voting/admin.py
@@ -5,7 +5,7 @@ from .models import Topic, Poll
 
 class TopicAdmin(admin.ModelAdmin):
     raw_id_fields = ("user",)
-    list_display = ("user", "name", "poll", "photo")
+    list_display = ("user", "name", "polls", "photo")
 
 
 admin.site.register(Topic, TopicAdmin)
